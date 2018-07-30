b2e98ddafc3ec5c00b01ed50f92b971e99d26fdd
Christopher Guindon <chris.guindon@eclipse-foundation.org>
2018-07-30 13:48:16 -0400

    update eclipsefdn-hugo-solstice-theme
    Change-Id: Ib56156b8d63e2a8f1b929eaaf4910531d1a83066
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse-foundation.org>

--- webpack.mix.js
+++ webpack.mix.js
@@ -22,7 +22,11 @@ mix.less('./node_modules/eclipsefdn-solstice-assets/less/quicksilver/jakarta/sty
 mix.scripts([
     './node_modules/jquery/dist/jquery.min.js',
     './node_modules/bootstrap/dist/js/bootstrap.min.js',
-    './node_modules/cookieconsent/build/cookieconsent.min.js',
+    './node_modules/jquery-match-height/dist/jquery.matchHeight-min.js',
     './node_modules/feather-icons/dist/feather.min.js',
+    './node_modules/cookieconsent/src/cookieconsent.js',
+    './node_modules/eclipsefdn-solstice-assets/js/solstice.cookieconsent.js',
+    './node_modules/eclipsefdn-solstice-assets/js/eclipsefdn.videos.js',
+    './node_modules/eclipsefdn-solstice-assets/js/solstice.cookies.js',
     './node_modules/eclipsefdn-solstice-assets/js/solstice.js'
-], 'static/js/solstice.js');
+], './static/js/solstice.js');
