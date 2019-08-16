f1bc612b24d7cf9a2a54864ec5bf469f268ce63b
Christopher Guindon (from Travis CI) <chris.guindon@eclipse-foundation.org>
2019-08-16 23:51:04 +0000

    Deploy jakarta.ee to github.com/jakartaee/jakarta.ee.git:master

---
+++ js/solstice.hugo.js
@@ -0,0 +1,1 @@
+!function(s,a){"undefined"!=typeof tableClasses&&null!=tableClasses&&s(a).ready(function(){s("main table").each(function(a){null==s(this).attr("class")&&s(this).addClass(tableClasses)})})}(jQuery,document);
