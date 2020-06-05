650cd8665592388996de626c4ad82c163312a3a8
Christopher Guindon (from Travis CI) <chris.guindon@eclipse-foundation.org>
2020-06-05 17:10:13 +0000

    Deploy jakarta.ee to github.com/jakartaee/jakarta.ee.git:master

--- js/solstice.hugo.js
+++ js/solstice.hugo.js
@@ -1,1 +1,15 @@
-!function(s,a){"undefined"!=typeof tableClasses&&null!=tableClasses&&s(a).ready(function(){s("main table").each(function(a){null==s(this).attr("class")&&s(this).addClass(tableClasses)})})}(jQuery,document);
+// only use this code if jquery has been defined
+(function($, document) {
+	// check to make sure that we have a table class to set
+	if (typeof tableClasses !== 'undefined' && tableClasses != null) {
+		// on document ready, inject table classes
+		$(document).ready(function() {
+			$('main table').each(function(index) {
+				// dont add classes if there are already some set
+				if ($(this).attr("class") == null) {
+					$(this).addClass(tableClasses);
+				}
+			});
+		});
+	}
+})(jQuery, document);
