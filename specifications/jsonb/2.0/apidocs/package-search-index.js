84d9a98b66bd3c6813813916bf1e84b28a117ea2
Christopher Guindon (from Travis CI) <chris.guindon@eclipse-foundation.org>
2020-06-10 13:47:33 +0000

    Deploy jakarta.ee to github.com/jakartaee/jakarta.ee.git:master

---
+++ specifications/jsonb/2.0/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"jakarta.json.bind"},{"l":"jakarta.json.bind.adapter"},{"l":"jakarta.json.bind.annotation"},{"l":"jakarta.json.bind.config"},{"l":"jakarta.json.bind.serializer"},{"l":"jakarta.json.bind.spi"}]
