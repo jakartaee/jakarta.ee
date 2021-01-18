01fca2c7ccdd2e52a87c66ff1246974433bd7106
Yi Liu <yi.liu@eclipse-foundation.org>
2021-01-18 11:42:56 -0500

    Update Theme to 0.0.111
    Signed-off-by: Yi Liu <yi.liu@eclipse-foundation.org>

--- webpack.mix.js
+++ webpack.mix.js
@@ -19,7 +19,7 @@ mix.setResourceRoot('../');
 
 mix.less('./less/styles.less', 'static/css/styles.css');
 
-mix.scripts([
+mix.babel([
     './node_modules/jquery/dist/jquery.min.js',
     './node_modules/bootstrap/dist/js/bootstrap.min.js',
     './node_modules/jquery-match-height/dist/jquery.matchHeight-min.js',
@@ -32,7 +32,7 @@ mix.scripts([
     './node_modules/eclipsefdn-solstice-assets/js/solstice.js'
 ], './static/js/solstice.js');
 
-mix.js(
+mix.babel(
     './js/contributor-cards/index.js',
     './static/js/release-page-cards.js'
 )
