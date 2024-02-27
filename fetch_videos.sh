cc722a49d92da6d774633d5c4de16d704b687f26
Olivier Goulet <olivier.goulet@eclipse-foundation.org>
2024-02-27 11:05:48 -0500

    feat: add relevant playlists

--- fetch_videos.sh
+++ fetch_videos.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 playlist_ids=(
-  "PLutlXcN4EAwCjJYYaPhFzJ31PqFRtOUFu" # Update Calls 
+  "PLutlXcN4EAwDCVHTlhymKrKBwSyddZoc8" # Update Calls 
   "PLutlXcN4EAwC64sgFLJSWAgQJvVo6T4Dh" # Tech Talks
+  "PLutlXcN4EAwAfZaPt8pdamYvE7APRcPbA" # Livestream 2024 PT
   "PLutlXcN4EAwDNN2lVNlfV3u2rBvJMOFCn" # Livestream 2023 EN
+  "PLutlXcN4EAwD_rW8juyr1ZQf7MEYRQuWb" # Livestream 2023 ES
+  "PLutlXcN4EAwCGe7GN9htD_QvANU9xTvrh" # Livestream 2023 ZH
 )
 
 mkdir -p ./data/videos
