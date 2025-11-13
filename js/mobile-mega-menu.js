9e48bcb0fcb6d5541b95f434546606cff338c4e0
Jordi Gómez <jordi.gomez@eclipse-foundation.org>
2025-11-13 13:30:34 +0100

    feat: updating license headers

--- js/mobile-mega-menu.js
+++ js/mobile-mega-menu.js
@@ -1,9 +1,9 @@
-/*!
- * Copyright (c) 2025 Eclipse Foundation, Inc.
+/**
+ * Copyright (c) 2025 Eclipse Foundation AISBL
  *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v. 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
  */
