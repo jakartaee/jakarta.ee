2c4ba185e97fb05a45d744cbef1a2bcdc00d3298
Jordi Gómez <jordi.gomez@eclipse-foundation.org>
2025-11-27 14:51:09 +0100

    feat: adding events section

--- js/main.js
+++ js/main.js
@@ -11,178 +11,140 @@
  * SPDX-License-Identifier: EPL-2.0
  */
 
-import 'eclipsefdn-solstice-assets'
-import List from 'list.js';
-import './video-modal';
-import eclipsefdnSpecificationBadges from './specification-badges';
-import { setupTopIntersection } from './top-intersection';
-import setupMobileMegaMenu from './mobile-mega-menu';
+import "eclipsefdn-solstice-assets";
+import List from "list.js";
+import "./video-modal";
+import eclipsefdnSpecificationBadges from "./specification-badges";
+import { setupTopIntersection } from "./top-intersection";
+import setupMobileMegaMenu from "./mobile-mega-menu";
 
-document.addEventListener("DOMContentLoaded", function() {
-    (function($, document) {
-      eclipsefdnSpecificationBadges.renderAll();
+document.addEventListener("DOMContentLoaded", function () {
+  (function ($, document) {
+    eclipsefdnSpecificationBadges.renderAll();
 
-      setupTopIntersection();
+    setupTopIntersection();
 
-      // Initialize Mobile Mega Menu
-      setupMobileMegaMenu();
+    // Initialize Mobile Mega Menu
+    setupMobileMegaMenu();
 
-      const matchHeightDropdownMenuItems = () => {
-        $('.match-height-dropdown-menu-item').matchHeight({
-          byRow: false
-        });
+    const matchHeightDropdownMenuItems = () => {
+      $(".match-height-dropdown-menu-item").matchHeight({
+        byRow: false,
+      });
+    };
+
+    const removeMatchHeightDropdownMenuItems = () => {
+      $(".match-height-dropdown-menu-item").matchHeight({
+        remove: true,
+      });
+    };
+
+    $(window).on("load", function () {
+      if ($(window).width() > 768) {
+        matchHeightDropdownMenuItems();
       }
+    });
 
-      const removeMatchHeightDropdownMenuItems = () => {
-        $('.match-height-dropdown-menu-item').matchHeight({
-          remove: true
-        });
+    $(window).on("resize", function () {
+      if ($(window).width() > 768) {
+        matchHeightDropdownMenuItems();
+      } else {
+        removeMatchHeightDropdownMenuItems();
       }
+    });
 
-      $(window).on("load", function() {
-        if ($(window).width() > 768) {
-          matchHeightDropdownMenuItems();
-        }
-      });
+    var owlSolsticeSlider = $(".solstice-slider");
+    owlSolsticeSlider.owlCarousel({
+      items: 1,
+      autoplay: true,
+      autoplayTimeout: 6000,
+      autoplayHoverPause: true,
+      autoplaySpeed: 2000,
+      loop: true,
+    });
 
-      $(window).on('resize', function () {
-        if ($(window).width() > 768) {
-          matchHeightDropdownMenuItems();
-        }
-        else {
-          removeMatchHeightDropdownMenuItems();
-        }
-      });
+    $(document).ready(function () {
+      $(".testimonial-container").html(
+        $(".testimonial-item")
+          .sort(function () {
+            return 0.5 - Math.random();
+          })
+          .slice(0, 3)
+          .slideDown("slow")
+      );
 
-      var owlSolsticeSlider = $('.solstice-slider');
-      owlSolsticeSlider.owlCarousel({
-          items:1,
-          autoplay:true,
-          autoplayTimeout:6000,
-          autoplayHoverPause:true,
-          autoplaySpeed: 2000,
-          loop:true,
-      });
-
-      const solsticeSliderHome = () => {
-        const owlSolsticeFeaturedStorySlider = $('.solstice-featured-story-slider');
-        
-        const initializeCarousel = () => {
-          owlSolsticeFeaturedStorySlider.owlCarousel({
-              items:1,
-              autoplay:true,
-              autoplayTimeout:6000,
-              autoplayHoverPause:true,
-              autoplaySpeed: 2000,
-              loop:true,
-          });
-        };
-
-        initializeCarousel();
-
-        // Setup a MutationObserver to observe whether the carousel has been
-        // populated. If populated, we want to initialize the owl carousel and
-        // stop observing.
-        // This fixes a problem where the owl carousel initializes before the
-        // data has been fetched - leading to UI issues.
-        const observer = new MutationObserver(() => {
-          const sliderElement = owlSolsticeFeaturedStorySlider.get(0);
-          const stageElement = sliderElement.querySelector('.owl-stage');
-          const isPopulated = stageElement?.length > 0;
-
-          // Stop observing if the carousel element has more than one child. We
-          // know that the data was fetched and populated if this is the case.
-          if (isPopulated) {
-            observer.disconnect();
-            return;
-          }
-
-          initializeCarousel();
-        });
-
-        observer.observe(owlSolsticeFeaturedStorySlider.get(0), {
-          childList: true,
-        });
-      }
-
-      $(window).on("load", function() {
-          solsticeSliderHome();
-      });
-
-      $(document).ready(function() {
-        $('.testimonial-container').html(
-          $(".testimonial-item").sort(function(){
-            return .5 - Math.random()
-          }).slice(0,3).slideDown("slow")
-        );
-
-        $('.features-and-benefits-btn').on('click', function(){
-          const parent = $(this).parent().parent();
-          $(parent).find('.features-and-benefits-text').slideToggle(300, function(){
+      $(".features-and-benefits-btn").on("click", function () {
+        const parent = $(this).parent().parent();
+        $(parent)
+          .find(".features-and-benefits-text")
+          .slideToggle(300, function () {
             if ($(this).is(":hidden")) {
-              $(parent).removeClass('match-height-item-active').addClass('match-height-item-closed');
-            }
-            else {
-              $(parent).removeClass('match-height-item-closed').addClass('match-height-item-active');
+              $(parent)
+                .removeClass("match-height-item-active")
+                .addClass("match-height-item-closed");
+            } else {
+              $(parent)
+                .removeClass("match-height-item-closed")
+                .addClass("match-height-item-active");
             }
             // Update match height if all items are active
             if ($(".feature-box.match-height-item-active").length > 1) {
-              $.fn.matchHeight._apply('.match-height-item-active');
+              $.fn.matchHeight._apply(".match-height-item-active");
             }
             if ($(".feature-box.match-height-item-closed").length > 1) {
-              $.fn.matchHeight._apply('.match-height-item-closed');
+              $.fn.matchHeight._apply(".match-height-item-closed");
             }
-            if ($(".feature-box.match-height-item-active").length === 0 || $(".feature-box.match-height-item-active").length == $(".feature-box").length) {
+            if (
+              $(".feature-box.match-height-item-active").length === 0 ||
+              $(".feature-box.match-height-item-active").length ==
+                $(".feature-box").length
+            ) {
               $.fn.matchHeight._update();
             }
           });
-          if ($(parent).is('[style]')) {
-            $(parent).removeAttr('style');
-          }
-          if ($(this).text().toLowerCase() === "learn more") {
-            $(this).text('Close');
-          }
-          else {
-            $(this).text('Learn more');
-          }
+        if ($(parent).is("[style]")) {
+          $(parent).removeAttr("style");
+        }
+        if ($(this).text().toLowerCase() === "learn more") {
+          $(this).text("Close");
+        } else {
+          $(this).text("Learn more");
+        }
+      });
+    });
+
+    $("#viewSpecificationSidebar").on("change", function () {
+      window.location.href = $("#viewSpecificationSelect").val();
+    });
+
+    // Initiate List.js for videos
+    const videosList = new List("videos-list", {
+      valueNames: ["category"],
+      page: 5,
+      pagination: true,
+      paginationClass: "pagination-videos",
+    });
+
+    let updateList = function () {
+      const category = $("#video-categories").val();
+      if (category == "none") {
+        videosList.filter();
+      } else {
+        videosList.filter(function (item) {
+          return category.includes(item.values().category) || !category;
         });
-      });
-
-
-      $('#viewSpecificationSidebar').on('change', function() {
-        window.location.href = $('#viewSpecificationSelect').val();
-      });
-
-      // Initiate List.js for videos
-      const videosList = new List('videos-list', {
-        valueNames: ['category'],
-        page: 5,
-        pagination: true,
-        paginationClass: "pagination-videos"
-      });
-
-      let updateList = function(){
-        const category = $("#video-categories").val();
-        if (category == 'none') {
-          videosList.filter();
-        }
-        else {
-          videosList.filter(function(item) {
-            return (category.includes(item.values().category) || !category);
-          });
-        }
-
-        // Replace youtube videos
-        eclipseFdnVideos.replace();
       }
 
-      // Update list after using the filter
-      $("#video-categories").on("change", updateList);
+      // Replace youtube videos
+      eclipseFdnVideos.replace();
+    };
 
-      // Replace youtube videos when pagination is clicked
-      $(document).on('click', 'a.page', function() {
-        eclipseFdnVideos.replace();
-      });
+    // Update list after using the filter
+    $("#video-categories").on("change", updateList);
 
-    })(jQuery, document);
+    // Replace youtube videos when pagination is clicked
+    $(document).on("click", "a.page", function () {
+      eclipseFdnVideos.replace();
+    });
+  })(jQuery, document);
 });
