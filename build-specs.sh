7ab154aeb9d7d65222ad6e9a35669ad121ac7adf
Eric Poirier <eric.poirier@eclipse-foundation.org>
2026-08-31 13:33:11 -0400

    Update comments
    Signed-off-by: Eric Poirier <eric.poirier@eclipse-foundation.org>

--- build-specs.sh
+++ build-specs.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # ===========================================================================
-# Copyright (c) 2019 Eclipse Foundation and others.
-# 
+# Copyright (c) 2026 Eclipse Foundation and others.
+#
 # This program and the accompanying materials are made
 # available under the terms of the Eclipse Public License 2.0
 # which is available at https://www.eclipse.org/legal/epl-2.0/
-# 
+#
 # Contributors:
-# Christopher Guindon (Eclipse Foundation)
-# 
+# Eric Poirier (Eclipse Foundation)
+#
 # SPDX-License-Identifier: EPL-2.0
 # ===========================================================================
 
