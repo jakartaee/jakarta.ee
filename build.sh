d31924807b6442ef89159e02a985b3600a5638f8
Eric Poirier <eric.poirier@eclipse-foundation.org>
2026-08-31 13:33:11 -0400

    Update comments
    Signed-off-by: Eric Poirier <eric.poirier@eclipse-foundation.org>

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 # ===========================================================================
-# Copyright (c) 2025 Eclipse Foundation and others.
+# Copyright (c) 2019 Eclipse Foundation and others.
 #
 # This program and the accompanying materials are made
 # available under the terms of the Eclipse Public License 2.0
 # which is available at https://www.eclipse.org/legal/epl-2.0/
 #
+# Contributors:
+# Christopher Guindon (Eclipse Foundation)
+#
 # SPDX-License-Identifier: EPL-2.0
+#
 # ===========================================================================
 
 set -o errexit
